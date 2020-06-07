5018a52d96fc7134a48227b3e049caab30a4bc28
fahad <hasher@localhost>
2020-06-08 02:29:44 +0530

    Connector.class cleanup

--- src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkConnector.java
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkConnector.java
@@ -16,57 +16,43 @@ import java.util.Map;
 public class KustoSinkConnector extends SinkConnector {
 
     private static final Logger log = LoggerFactory.getLogger(KustoSinkConnector.class);
-    private Map<String, String> configProps;
-
-    public KustoSinkConnector() {
-        // No-arg constructor. It is instantiated by Connect framework.
-        log.info("KustoSinkConnector initialized");
-    }
-
-    KustoSinkConnector(KustoSinkConfig config) {
-        // No-arg constructor. It is instantiated by Connect framework.
-        log.info("KustoSinkConnector initialized with config");
-    }
+    
+    private KustoSinkConfig config;
 
     @Override
-    public String version() {
-        return Version.getVersion();
+    public void start(Map<String, String> props) {
+        log.info("Starting KustoSinkConnector.");
+        config = new KustoSinkConfig(props);
+    }
+    
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        List<Map<String, String>> configs = new ArrayList<>();
+        Map<String, String> taskProps = new HashMap<>();
+        taskProps.putAll(config.originalsStrings());
+        for (int i = 0; i < maxTasks; i++) {
+          configs.add(taskProps);
+        }
+        return configs;
+    }
+    
+    @Override
+    public void stop() {
+        log.info("Shutting down KustoSinkConnector");
+    }
+    
+    @Override
+    public ConfigDef config() {
+        return KustoSinkConfig.getConfig();
     }
 
     @Override
     public Class<? extends Task> taskClass() {
         return KustoSinkTask.class;
     }
-
-
+    
     @Override
-    public void start(Map<String, String> props) throws ConnectException {
-        try {
-            configProps = new HashMap<>(props);
-        } catch (ConfigException e) {
-            throw new ConnectException("Couldn't start KustoSinkConnector due to configuration error", e);
-        }
-    }
-
-    @Override
-    public List<Map<String, String>> taskConfigs(int maxTasks) {
-        Map<String, String> taskProps = new HashMap<>(configProps);
-        List<Map<String, String>> taskConfigs = new ArrayList<>();
-
-        for (int i = 0; i < maxTasks; i++) {
-            taskConfigs.add(taskProps);
-        }
-
-        return taskConfigs;
-    }
-
-    @Override
-    public void stop() throws ConnectException {
-        log.info("Shutting down KustoSinkConnector");
-    }
-
-    @Override
-    public ConfigDef config() {
-        return KustoSinkConfig.getConfig();
+    public String version() {
+        return Version.getVersion();
     }
 }
