1b0230829730a1908944710fdff6e5e701b57ae7
hasher <anmol.bansal@hashedin.com>
2020-06-21 16:01:24 +0530

    clean the code

--- src/test/java/com/microsoft/azure/kusto/kafka/connect/sink/E2ETest.java
+++ src/test/java/com/microsoft/azure/kusto/kafka/connect/sink/E2ETest.java
@@ -24,11 +24,11 @@ import java.util.logging.Logger;
 
 public class E2ETest {
     private static final String testPrefix = "tmpKafkaE2ETest";
-    private String appId = System.getProperty("appId","e299f8c0-4965-4b1f-9b55-a56dd4b8f6c4");
-    private String appKey = System.getProperty("appKey",".1t-fP8o~a0b8X7-o0.wCr6.0aPBR7~k9L");
-    private String authority = System.getProperty("authority","7f66d0ea-6137-4e37-a835-4530eba9b3ee");
-    private String cluster = System.getProperty("cluster","azureconnector.centralus");
-    private String database = System.getProperty("database","anmol");
+    private String appId = System.getProperty("appId","xxx");
+    private String appKey = System.getProperty("appKey","xxx");
+    private String authority = System.getProperty("authority","xxxx");
+    private String cluster = System.getProperty("cluster","xxxx");
+    private String database = System.getProperty("database","xxxx");
     private String tableBaseName = System.getProperty("table", testPrefix + UUID.randomUUID().toString().replace('-', '_'));
     private String basePath = Paths.get("src/test/resources/", "testE2E").toString();
     private Logger log = Logger.getLogger(this.getClass().getName());
