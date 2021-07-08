856679f2dbd88656a09c4ed5cd121d25a43eeb3c
ohad bitton <32278684+ohadbitt@users.noreply.github.com>
2021-07-08 17:41:58 +0300

    Use sdk 2.8.2 (#57)
    * sdk 2.8.2
    
    * sdk 2.8.2
    
    Co-authored-by: KustoIbizaExtension Build <kustodev@microsoft.com>

--- src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkTask.java
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkTask.java
@@ -2,6 +2,7 @@ package com.microsoft.azure.kusto.kafka.connect.sink;
 
 import com.google.common.base.Strings;
 import com.microsoft.azure.kusto.data.*;
+import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder;
 import com.microsoft.azure.kusto.data.exceptions.DataClientException;
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException;
 import com.microsoft.azure.kusto.ingest.IngestClient;
