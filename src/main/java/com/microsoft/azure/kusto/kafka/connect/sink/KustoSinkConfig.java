83f579f62df99fc87f0fd3b4331e1b53ffbca64b
Ramachandran A G <106139410+ag-ramachandran@users.noreply.github.com>
2022-09-07 14:54:13 +0530

    Feature/update readme docs (#84)
    * Update README.md for PR-81/82/83
    * Make table validations as explicit opt out

--- src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkConfig.java
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/KustoSinkConfig.java
@@ -309,7 +309,7 @@ public class KustoSinkConfig extends AbstractConfig {
             .define(
                 KUSTO_SINK_ENABLE_TABLE_VALIDATION,
                 Type.BOOLEAN,
-                Boolean.FALSE,
+                Boolean.TRUE,
                 Importance.LOW,
                 KUSTO_SINK_ENABLE_TABLE_VALIDATION_DOC,
                 connectionGroupName,
