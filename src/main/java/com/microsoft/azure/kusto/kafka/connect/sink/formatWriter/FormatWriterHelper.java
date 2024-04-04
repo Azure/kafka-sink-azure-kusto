8a5ce9f2b5831e5e4150fe481d5c510e3bf8723f
Ramachandran A G <ramacg@microsoft.com>
2024-04-04 19:25:13 +0530

    * Fix conflicts

---
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/formatWriter/FormatWriterHelper.java
@@ -0,0 +1,101 @@
+package com.microsoft.azure.kusto.kafka.connect.sink.formatWriter;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.connect.avro.AvroData;
+import io.confluent.kafka.serializers.NonRecordContainer;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.*;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.kafka.connect.data.Schema;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.avro.file.DataFileConstants;
+import org.jetbrains.annotations.Nullable;
+
+public class FormatWriterHelper {
+    public static String KEY_FIELD = "key";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
+    private static final AvroData AVRO_DATA = new AvroData(50);
+
+    private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE
+            = new TypeReference<Map<String, Object>>() {
+    };
+    private FormatWriterHelper() {
+    }
+
+    public static @NotNull Map<String, Object> convertAvroRecordToMap(Schema schema, Object value) throws IOException {
+        Map<String, Object> updatedValue = new HashMap<>();
+        if (value != null) {
+            if (value instanceof NonRecordContainer) {
+                updatedValue.put(schema.name(), ((NonRecordContainer) value).getValue());
+            } else {
+                if (value instanceof GenericData.Record) {
+                    org.apache.avro.Schema avroSchema = AVRO_DATA.fromConnectSchema(schema);
+                    updatedValue.putAll(extractGenericDataRecord(value, avroSchema));
+                }
+            }
+        }
+        return updatedValue;
+    }
+
+    public static @NotNull Map<String, Object> convertBytesToMap(byte[] messageBytes) throws IOException {
+        GenericRecord genericRecord = bytesToAvroRecord(messageBytes);
+        if (genericRecord != null) {
+            return convertAvroRecordToMap(AVRO_DATA.toConnectSchema(genericRecord.getSchema()), genericRecord);
+        } else {
+            return Collections.singletonMap(KEY_FIELD, Base64.getEncoder().encodeToString(messageBytes));
+        }
+    }
+
+    private static Map<String, Object>  extractGenericDataRecord(Object value, org.apache.avro.Schema avroSchema) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            JsonEncoder encoder = EncoderFactory.get().jsonEncoder(avroSchema, baos);
+            DatumWriter<Object> writer = new GenericDatumWriter<>(avroSchema);
+            writer.write(value, encoder);
+            encoder.flush();
+            return OBJECT_MAPPER.readerFor(MAP_TYPE_REFERENCE).readValue(baos.toByteArray());
+        }
+    }
+
+    public static @NotNull Map<String, Object> convertStringToMap(Object value) throws IOException {
+        String objStr = (String) value;
+        if(isJson(objStr)) {
+            return OBJECT_MAPPER.readerFor(MAP_TYPE_REFERENCE).readValue(objStr);
+        } else {
+            return Collections.singletonMap(KEY_FIELD, objStr);
+        }
+    }
+    private static boolean isJson(String json) {
+        try {
+            OBJECT_MAPPER.readTree(json);
+        } catch (JacksonException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static @Nullable GenericRecord bytesToAvroRecord(byte[] received_message) throws IOException {
+        if(ArrayUtils.isEmpty(received_message)){
+           return null;
+        }
+        if (received_message.length < DataFileConstants.MAGIC.length ) {
+            return null;
+        }
+        if (Arrays.equals(DataFileConstants.MAGIC, Arrays.copyOf(received_message, DataFileConstants.MAGIC.length))) {
+            DatumReader<GenericRecord> avroBytesReader = new GenericDatumReader<>();
+            Decoder decoder = DecoderFactory.get().binaryDecoder(received_message, null);
+            return avroBytesReader.read(null, decoder);
+        }
+        return null;
+    }
+}
--- src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/formatWriter/avro/AvroRecordWriter.java
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/formatWriter/avro/AvroRecordWriter.java
@@ -3,14 +3,9 @@ package com.microsoft.azure.kusto.kafka.connect.sink.formatWriter.avro;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.kusto.kafka.connect.sink.format.RecordWriter;
-import com.microsoft.azure.kusto.kafka.connect.sink.format.RecordWriterProvider;
+import com.microsoft.azure.kusto.kafka.connect.sink.formatWriter.FormatWriterHelper;
 import com.microsoft.azure.kusto.kafka.connect.sink.formatWriter.HeaderAndMetadataWriter;
-import io.confluent.connect.avro.AvroData;
-import io.confluent.kafka.serializers.NonRecordContainer;
-import org.apache.avro.generic.GenericData;
-import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.sink.SinkRecord;
@@ -24,14 +19,17 @@ import java.util.Map;
 
 
 public class AvroRecordWriter extends HeaderAndMetadataWriter implements RecordWriter {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final Logger LOGGER = LoggerFactory.getLogger(AvroRecordWriter.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String LINE_SEPARATOR = System.lineSeparator();
-    private final AvroData AVRO_DATA = new AvroData(50);
     private final String filename;
     private final JsonGenerator writer;
     private Schema schema;
+
+    private final OutputStream out;
+
     public AvroRecordWriter(String filename, OutputStream out) {
+        this.out = out;
         this.filename = filename;
         try {
             this.writer = OBJECT_MAPPER.getFactory()
@@ -41,35 +39,16 @@ public class AvroRecordWriter extends HeaderAndMetadataWriter implements RecordW
             throw new ConnectException(e);
         }
     }
+
     @Override
     public void write(SinkRecord record) throws IOException {
         try {
-            Map<String, Object> updatedValue = new HashMap<>();
             if (schema == null) {
                 schema = record.valueSchema();
                 LOGGER.debug("Opening record writer for: {}", filename);
-                org.apache.avro.Schema avroSchema = AVRO_DATA.fromConnectSchema(schema);
             }
-            Object messageValue = record.value() == null ? null : AVRO_DATA.fromConnectData(schema, record.value());
-            // AvroData wraps primitive types so their schema can be included. We need to unwrap
-            // NonRecordContainers to just their value to properly handle these types
-            if (messageValue != null) {
-                if (messageValue instanceof NonRecordContainer) {
-                    updatedValue.put(schema.name(), ((NonRecordContainer) messageValue).getValue());
-                } else {
-                    if (schema != null) {
-                        for (Field field : schema.fields()) {
-                            if (messageValue instanceof Struct) {
-                                updatedValue.put(field.name(), ((Struct) messageValue).get(field));
-                            } else if (messageValue instanceof GenericData.Record) {
-                                updatedValue.put(field.name(), ((GenericData.Record) messageValue).get(field.name()));
-                            } else {
-                                throw new DataException("Unsupported record type: " + messageValue.getClass());
-                            }
-                        }
-                    }
-                }
-            }
+            Object messageValue = record.value();
+            Map<String, Object> updatedValue = new HashMap<>(FormatWriterHelper.convertAvroRecordToMap(schema, messageValue));
             updatedValue.put(KEYS_FIELD, getKeysMap(record));
             updatedValue.put(KAFKA_METADATA_FIELD, getKafkaMetaDataAsMap(record));
             writer.writeObject(updatedValue);
@@ -78,6 +57,7 @@ public class AvroRecordWriter extends HeaderAndMetadataWriter implements RecordW
             throw new ConnectException(e);
         }
     }
+
     @Override
     public void close() {
         try {
@@ -86,6 +66,7 @@ public class AvroRecordWriter extends HeaderAndMetadataWriter implements RecordW
             throw new DataException(e);
         }
     }
+
     @Override
     public void commit() {
         try {
