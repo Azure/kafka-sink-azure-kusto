8adf3380a000bd0885ea347d623662769b6df8a9
Ramachandran A G <ramacg@microsoft.com>
2024-04-16 20:40:37 +0530

    * Changes for Generic format writer

---
+++ src/main/java/com/microsoft/azure/kusto/kafka/connect/sink/formatwriter/StringRecordWriterProvider.java
@@ -0,0 +1,49 @@
+package com.microsoft.azure.kusto.kafka.connect.sink.formatwriter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.microsoft.azure.kusto.kafka.connect.sink.format.RecordWriter;
+import com.microsoft.azure.kusto.kafka.connect.sink.format.RecordWriterProvider;
+
+public class StringRecordWriterProvider implements RecordWriterProvider {
+    private static final Logger log = LoggerFactory.getLogger(StringRecordWriterProvider.class);
+
+    @Override
+    public RecordWriter getRecordWriter(String filename, OutputStream out) {
+        return new RecordWriter() {
+
+            @Override
+            public void write(SinkRecord record) throws IOException {
+                byte[] value = null;
+                value = String.format("%s\n", record.value()).getBytes(StandardCharsets.UTF_8);
+                out.write(value);
+            }
+
+            @Override
+            public void close() {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    throw new DataException(e);
+                }
+            }
+
+            @Override
+            public void commit() {
+                try {
+                    out.flush();
+                } catch (IOException e) {
+                    throw new DataException(e);
+                }
+            }
+        };
+    }
+
+}
